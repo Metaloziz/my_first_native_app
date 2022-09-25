7b55be9a3661e0513c27f5000e8e77b9ea3c2247
Andrew Gaityukevich <andrewgaity@yandex.by>
2022-09-25 12:00:00 +0300

    feat: 3 lesson

--- App.js
+++ App.js
@@ -3,7 +3,21 @@ import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (<View style={styles.container}>
-    <Text>Hello world ! From iOS</Text>
+    <View style={styles.header}>
+      <Text style={styles.boldText}>Hello world ! From iOS</Text>
+    </View>
+    <View style={styles.body}>
+      <Text style={styles.boldText}>Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Alias commodi
+        consequuntur <Text>distinctio esse explicabo fugiat illum ipsum iste laborum
+          laudantium</Text>
+        nam nesciunt nihil, quasi qui ratione, repudiandae tempore vero
+        voluptatibus.</Text>
+      <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias commodi
+        consequuntur distinctio esse explicabo fugiat illum ipsum iste laborum laudantium
+        nam nesciunt nihil, quasi qui ratione, repudiandae tempore vero
+        voluptatibus.</Text>
+    </View>
     <StatusBar style="auto"/>
   </View>);
 }
@@ -11,5 +25,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
+  }, header: {
+    backgroundColor: 'pink', padding: 20,
   },
+
+  boldText: {
+    fontWeight: 'bold'
+  },
+
+  body: {
+    backgroundColor: 'yellow', padding: 20,
+
+  }
+
 });
