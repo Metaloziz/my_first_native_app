5e2f05a9f3babfa7d45b4809eb15a07fe2cc6686
Andrew Gaityukevich <andrewgaity@yandex.by>
2022-09-28 18:25:20 +0300

    feat: #7 - Flat List Component

--- App.js
+++ App.js
@@ -1,36 +1,57 @@
-import { StatusBar } from 'expo-status-bar'
-import { useState } from 'react'
-import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import {StatusBar} from 'expo-status-bar'
+import {useState} from 'react'
+import {
+  Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View
+} from 'react-native'
 
 
 export default function App() {
 
-  const [people, setPeople] = useState([{ name: '1', key: '1' }, {
-    name: '2',
-    key: '2'
-  }, { name: '3', key: '3' }, { name: '4', key: '4' }, {
-    name: '5',
-    key: '5'
-  }, { name: '6', key: '6' }, { name: '7', key: '7' },])
+  const [people, setPeople] = useState([
+    {name: '1', id: '1'},
+    {name: '2', id: '2'},
+    {name: '3', id: '3'},
+    {name: '4', id: '4'},
+    {name: '5', id: '5'},
+    {name: '6', id: '6'},
+    {name: '7', id: '7'},
+  ])
 
 
   return (<View style={styles.container}>
-    <ScrollView>
-      {people.map((el) =>
-        <View key={el.key}><Text style={styles.item}>{el.name}</Text></View>)}
-    </ScrollView>
+
+    <FlatList data={people}
+              numColumns={3}
+              keyExtractor={(item) => item.id}
+              renderItem={
+                ({item}) => <Text style={styles.item}>{item.name}</Text>
+              }
+    />
+
+    {/*<ScrollView>*/}
+    {/*{people.map((el) =>*/}
+    {/*  <View key={el.key}><Text style={styles.item}>{el.name}</Text></View>)}*/}
+    {/*</ScrollView>*/}
   </View>)
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, backgroundColor: '#fff', paddingTop: 40, paddingHorizontal: 20
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+
     // alignItems: 'center',
     // justifyContent: 'center',
   },
 
   item: {
-    marginTop: 25, padding: 30, backgroundColor: 'green', fontSize: 20
+    marginTop: 25,
+    padding: 30,
+    backgroundColor: 'green',
+    fontSize: 20,
+    marginHorizontal: 10
   }
 
 
